1e0e07b93e8a40be98e7ab238bc21f17e1dc4616
GillAlexander <jonte962010@hotmail.com>
2019-01-15 21:32:26 +0100

    minor

---
+++ Pressing Matters/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input)
+        {
+
+        }
+    }
+}
